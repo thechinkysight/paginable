2484ea6d4e644d0e0102eacde7fc123d9c721cf4
Chinky Sight <53231016+chinkysight@users.noreply.github.com>
2021-10-14 11:00:39 +0545

    Added PaginableListView() widget
    Added documentation comments to PaginableListView()
    
    Restricted the use of ValueNotifer after it has been disposed

--- lib/paginable.dart
+++ lib/paginable.dart
@@ -2,3 +2,5 @@ export 'src/paginable_listview_builder.dart';
 
 export 'src/paginable_sliver_child_builder_delegate.dart';
 export 'src/paginable_custom_scroll_view.dart';
+
+export 'src/paginable_listview.dart';
